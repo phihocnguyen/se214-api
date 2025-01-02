f809b610bd0a982e005964c50c800348b3a3f399
ch1ta <ng.phihoc123@gmail.com>
2025-01-02 13:33:37 +0700

    refactor(payment): add file upload middleware

--- src/routes/payment.js
+++ src/routes/payment.js
@@ -1,11 +1,11 @@
 const express = require('express')
-
+const { upload } = require('../middlewares/multer')
 const { createPayment, getPayments, updatePayment } = require('../controllers/payment')
 
 
 const router = express.Router()
 
-router.post('/', createPayment)
+router.post('/', upload.single('file'), createPayment)
 router.get('/', getPayments)
 router.patch('/:id', updatePayment)
 exports.paymentRoutes = router
